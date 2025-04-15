c8b64e2c119d98f4f153f0d5d0f4c53534574d73
Abhishek0020 <90542072+Abhishek0020@users.noreply.github.com>
2025-04-15 12:54:06 +0530

    Time: 9 ms (34%), Space: 44.9 MB (18.46%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        s = s.toLowerCase();
+        t = t.toLowerCase();
+
+        while (true) {
+            if (s.length() != t.length())
+                return false;
+            else if (s.length() == 0 && t.length() == 0)
+                return true;
+
+            char ch = s.charAt(0);
+            s = s.replace(ch + "", "");
+            t = t.replace(ch + "", "");
+        }
+    }
+}
