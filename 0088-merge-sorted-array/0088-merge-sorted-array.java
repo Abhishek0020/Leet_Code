cadcac7df7e1a45898011466798f0c13206eb044
Abhishek0020 <90542072+Abhishek0020@users.noreply.github.com>
2025-07-14 17:10:13 +0530

    Time: 0 ms (100%), Space: 42.1 MB (78.11%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,10 +1,44 @@
+import java.util.Arrays;
+
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-     
-       for(int i=0;i<n;i++){
-            nums1[m]=nums2[i];
-            m=m+1;
-       }
-        Arrays.sort(nums1);  
+        // Pointers for nums1, nums2, and merged position
+        int i = m - 1;       // Last index of elements in nums1
+        int j = n - 1;       // Last index of nums2
+        int k = m + n - 1;   // Last index of merged array
+
+        // Merge nums1 and nums2 from end to beginning
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        // If any elements remain in nums2
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+
+    // For testing
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+
+        sol.merge(nums1, m, nums2, n);
+
+        System.out.println("Merged Array: " + Arrays.toString(nums1));
     }
 }
