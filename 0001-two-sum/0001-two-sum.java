802487cd854ffeb9bb0bd3183054c9852b9ac30b
Abhishek0020 <90542072+Abhishek0020@users.noreply.github.com>
2025-07-23 21:18:53 +0530

    Time: 3 ms (61.58%), Space: 45.2 MB (30.56%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,56 +1,26 @@
-public class Solution {
-
-    public static int[] twoSum(int[] nums, int target) {
-        // Loop through the array with two pointers
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                // Check if the sum of nums[i] and nums[j] equals the target
-                if (nums[i] + nums[j] == target) {
-                    return new int[] { i, j };  // Return the indices of the two numbers
-                }
-            }
-        }
+import java.util.HashMap;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
         
-        // If no solution is found, return an empty array (though in a valid problem there should always be a solution)
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=1;j<nums.length;j++){
+        //         if(nums[i]+nums[j]==target){
+        //             return new int[] {i,j};
+        //         }
+        //     }
+        // }
+        // return new int[] {};
+
+
+        HashMap<Integer, Integer>map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int complement=target-nums[i];
+
+            if(map.containsKey(complement)){
+                return new int[] {map.get(complement) ,i};
+            }
+            map.put(nums[i],i);
+        }
         return new int[] {};
     }
-
-    public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15};
-        int target = 9;
-        
-        int[] result = twoSum(nums, target);
-        
-        System.out.println("Indices of the two numbers are: [" + result[0] + ", " + result[1] + "]");
-    }
 }
-
-
-        
-    //     // Traverse through the array
-    //     for (int i = 0; i < nums.length; i++) {
-    //         for (int j = i + 1; j < nums.length; j++) {
-    //             // If the sum of nums[i] and nums[j] equals the target
-    //             if (nums[i] + nums[j] == target) {
-    //                 return new int[] {i, j};  // Return the indices of the two numbers
-    //             }
-    //         }
-    //     }
-    //     // Return an empty array if no solution is found (though the problem assumes there's always a solution)
-    //     return new int[] {};
-    // }
-
-    // public static void main(String[] args) {
-    //     int[] nums = {2, 7, 11, 15};
-    //     int target = 9;
-
-    //     int[] result = twoSum(nums, target);
-        
-    //     if (result.length == 2) {
-    //         System.out.println("Indices: " + result[0] + ", " + result[1]);
-    //     } else {
-    //         System.out.println("No solution found.");
-    //     }
-
-
-    
