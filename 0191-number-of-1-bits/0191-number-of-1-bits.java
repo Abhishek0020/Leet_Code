35d7e331e7320ef5079671ffd5a5691081e34d7a
Abhishek0020 <90542072+Abhishek0020@users.noreply.github.com>
2026-01-22 16:55:14 +0530

    Time: 0 ms (100%), Space: 42.1 MB (96.51%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,16 +1,10 @@
-public class Solution {
-    
+class Solution {
     public int hammingWeight(int n) {
-        int count=0;
-       while(n !=0){
-           
-           count +=n&1;
-           n>>>=1;
-           
-       }
-        
+        int count = 0;
+        while (n != 0) {
+            n = n & (n - 1); // removes the lowest set bit
+            count++;
+        }
         return count;
-        
-        
     }
 }
