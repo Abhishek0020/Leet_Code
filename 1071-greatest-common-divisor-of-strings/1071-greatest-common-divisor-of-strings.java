df62e0981c136f4bd2affee95f809fcf35899149
Abhishek0020 <90542072+Abhishek0020@users.noreply.github.com>
2026-03-18 22:03:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -1,21 +1,14 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-
-        // Step 1: check if valid pattern exists
-        if (!(str1 + str2).equals(str2 + str1)) {
+        if(!(str1+str2).equals(str2+str1)){
             return "";
         }
 
-        // Step 2: find gcd of lengths
-        int len = gcd(str1.length(), str2.length());
-
-        // Step 3: return substring
-        return str1.substring(0, len);
+        int len=gcd(str1.length(),str2.length());
+        return str1.substring(0,len);
     }
-
-    // helper method (outside main method)
-    public int gcd(int a, int b) {
-        if (b == 0) return a;
-        return gcd(b, a % b);
+    public int gcd(int a,int b){
+        if(b==0) return a;
+        return gcd(b,a%b);
     }
 }
